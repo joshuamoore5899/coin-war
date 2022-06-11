52c3b2674f933637a8a0bdf4a2b63e0972b82746
joshuamoore5899 <joshuamoore5899@gmail.com>
2022-06-10 21:12:29 -0600

    adding to heroku

---
+++ js/main.js
@@ -0,0 +1,89 @@
+document.querySelector('#heads').addEventListener('click', heads);
+document.querySelector('#tails').addEventListener('click', tails);
+document.querySelector('#reset').addEventListener('click', reset);
+
+
+
+if (localStorage.getItem('count')) {
+  console.log('Yes');
+  let count = localStorage.getItem('count')
+  document.querySelector('#score').innerText = count;
+}
+else {
+  let count = 0;
+  console.log('No');
+  localStorage.setItem('count', count);
+  document.querySelector('#score').innerText = count;
+}
+
+
+if (localStorage.getItem('computer-count')) {
+  console.log('Yes');
+  let computerCount = localStorage.getItem('computer-count');
+  document.querySelector('#computer-score').innerText = computerCount;
+}
+else {
+  let computerCount = 0;
+  console.log('No');
+  localStorage.setItem('computer-count', computerCount);
+  document.querySelector('#computer-score').innerText = computerCount;
+}
+
+
+function reset() {
+  console.log('in reset')
+  count = 0;
+  computerCount = 0;
+  localStorage.setItem('count', count);
+  localStorage.setItem('computer-count', computerCount);
+  document.querySelector('#score').innerText = count;
+  document.querySelector('#computer-score').innerText = computerCount;
+}
+
+async function heads() {
+    document.querySelector('span').innerText = "";
+    const userGuess = 'HEADS';
+    const res = await fetch('/api');
+    const data = await res.json();
+
+    let result = data.result;
+    document.querySelector('span').innerText = result;
+    if (result === userGuess) {
+      document.querySelector('h3').innerText = 'You win!';
+      count = localStorage.getItem('count');
+      count++;
+      localStorage.setItem('count', count);
+      document.querySelector('#score').innerText = count;
+    }
+    else {
+      document.querySelector('h3').innerText = 'You lose!';
+      computerCount = localStorage.getItem('computer-count');
+      computerCount++;
+      localStorage.setItem('computer-count', computerCount);
+      document.querySelector('#computer-score').innerText = computerCount;
+    }
+}
+
+async function tails() {
+    document.querySelector('span').innerText = "";
+    const userGuess = 'TAILS';
+    const res = await fetch('/api');
+    const data = await res.json();
+
+    let result = data.result;
+    document.querySelector('span').innerText = result;
+    if (result === userGuess) {
+      document.querySelector('h3').innerText = 'You win!';
+      count = localStorage.getItem('count');
+      count++;
+      localStorage.setItem('count', count);
+      document.querySelector('#score').innerText = count;
+    }
+    else {
+      document.querySelector('h3').innerText = 'You lose!';
+      computerCount = localStorage.getItem('computer-count');
+      computerCount++;
+      localStorage.setItem('computer-count', computerCount);
+      document.querySelector('#computer-score').innerText = computerCount;
+    }
+}
---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = 8000;
+
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/js/main.js', (req, res) => {
+  res.sendFile(__dirname + '/js/main.js')
+})
+
+app.get('/css/style.css', (req, res) => {
+  res.sendFile(__dirname + '/css/style.css')
+})
+
+app.get('/api', (req, res) => {
+  let result = Math.random() * 100;
+  let ans = "";
+  if (result < 50) {
+    ans = "HEADS";
+  }
+  else {
+    ans = "TAILS";
+  }
+  const objToJson = {
+    result: ans,
+  }
+  res.json(objToJson);
+})
+
+// const server = http.createServer((req, res) => {
+//   const page = url.parse(req.url).pathname;
+//   const params = querystring.parse(url.parse(req.url).query);
+//   if (page === '/') {
+//     fs.readFile('index.html', function (err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }
+//   else if (page === '/api') {
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     let result = Math.random() * 100;
+//     let ans = "";
+//     if (result < 50) {
+//       ans = "HEADS";
+//     }
+//     else {
+//       ans = "TAILS";
+//     }
+//     const objToJson = {
+//       result: ans,
+//     }
+//     res.end(JSON.stringify(objToJson));
+//   }
+//   else if (page === '/css/style.css') {
+//     fs.readFile('css/style.css', function(err, data) {
+//       res.write(data);
+//       res.end();
+//     })
+//   }
+//   else if (page === '/js/main.js') {
+//     fs.readFile('js/main.js', function (err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/javascript'});
+//       res.write(data);
+//       res.end();
+//     })
+//   }
+//   else {
+//     figlet('404 !', function(err, data) {
+//       if (err) {
+//         console.log('Error');
+//         console.dir(err);
+//         return;
+//       }
+//       res.write(data);
+//       res.end();
+//     })
+//   }
+// })
+
+app.listen(process.env.PORT || PORT, ()=>{
+  console.log(`Listening on port ${PORT}`)
+})
